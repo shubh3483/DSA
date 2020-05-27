f58a601a34355e316629086c263a910009921b81
Shubh Agrawal <shubh.coding@gmail.com>
2020-05-27 13:10:35 +0530

    Create bubble sort.cpp

---
+++ bubble sort.cpp
@@ -0,0 +1,39 @@
+#include<iostream.h>
+#include<conio.h>
+#define max 10
+int a[max];
+void bubble(int n)
+{int i,j,t;
+cout<<"enter elements";
+for(i=0;i<n;i++)
+   {
+   cin>>a[i];
+   }
+for(i=0;i<n-1;i++)
+    {for(j=0;j<n-1-i;j++)
+	{if(a[j]>a[j+1])
+	    {	t=a[j+1];
+		a[j+1]=a[j];
+		a[j]=t;
+	    }
+	}
+    }
+}
+int display(int n)
+{int i;
+cout<<"elements in sorted order are"<<endl;
+for(i=0;i<n;i++)
+	{cout<<a[i]<<endl;
+	}
+return 0;
+}
+int main()
+{clrscr();
+int b;
+cout<<"enter number of elements";
+cin>>b;
+bubble(b);
+display(b);
+getch();
+return 0;
+}
